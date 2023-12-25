5329337765ae754ed309eee5b451021236537887
Djordje Savanovic <ds21546@essex.ac.uk>
2023-12-25 20:02:41 +0100

    - Fix for model_type being sent to the encoder

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ CORS(app)  # Enable CORS for the Flask app. This makes the API accessible to web
 encoder = LabelEncoder()
 encoder.classes_ = np.load('classes.npy', allow_pickle=True)  # Load the classes used by the LabelEncoder.
 
+
 # Function to encode features before making a prediction.
 def encode_features(input_data, encoder):
     # Define the list of categorical features that need to be encoded.
@@ -35,6 +36,7 @@ def encode_features(input_data, encoder):
             encoded_data[feature] = -1
     return encoded_data  # Return the encoded data.
 
+
 # Load the LightGBM model from the serialized file.
 model_file_path = 'best_lgbm_model.pkl'
 with open(model_file_path, 'rb') as file:  # Open the file in read-binary mode.
@@ -44,19 +46,23 @@ model_file_path = 'best_lgbm_model_no_outliers.pkl'
 with open(model_file_path, 'rb') as file:  # Open the file in read-binary mode.
     lgbm_model_no_outliers = pickle.load(file)  # Load the trained model.
 
+
 # Define a route for the root URL which returns a simple string.
 @app.route('/')
 def home():
     return "LightGBM Model API"
 
+
 # Define the route for making predictions using a POST request.
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
+    data = request.get_json(force=True)  # Get JSON data from the POST request.
 
     try:
-        # Check which model to use based on 'model_type' parameter
-        model_type = data.get('model_type')
+        # Extract the model_type parameter from the request
+        model_type = data.pop('model_type', None)
+
+        # Select the appropriate model based on model_type
         if model_type == 'model_1':
             model = lgbm_model_with_outliers
         elif model_type == 'model_2':
@@ -65,13 +71,14 @@ def predict():
             return jsonify({"error": "Invalid model type"}), 400
 
         # Encode features and make prediction
-        encoded_inputs = encode_features(data, encoder)
+        encoded_inputs = encode_features(data, encoder)  # data no longer contains 'model_type'
         features = np.array([list(encoded_inputs.values())])
         prediction = model.predict(features)
         return jsonify(prediction.tolist())
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
 
 # If this script is run directly, start the Flask web server.
 # Set debug=True for development, so the server will reload on code changes.
