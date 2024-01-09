5a42884d149a92f9a42b30558e5b0c11411f3c68
Djordje Savanovic <ds21546@essex.ac.uk>
2024-01-09 20:10:52 +0100

    - Experiment with model

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ CORS(app)  # Enable CORS for the Flask app. This makes the API accessible to web
 encoder = LabelEncoder()
 encoder.classes_ = np.load('classes.npy', allow_pickle=True)  # Load the classes used by the LabelEncoder.
 
-
-# Function to encode features before making a prediction.
 def encode_features(input_data, encoder):
-    # Define the list of categorical features that need to be encoded.
     categorical_features = [
         'IM_INCIDENT_KEY', 'FIRE_BOX', 'STREET_HIGHWAY', 'ZIP_CODE',
         'INCIDENT_TYPE_DESC', 'PROPERTY_USE_DESC', 'BOROUGH_DESC', 'FLOOR'
@@ -25,14 +22,12 @@ def encode_features(input_data, encoder):
     # Iterate over the categorical features and encode them.
     for feature in categorical_features:
         if encoded_data[feature] != "":
-            # If the feature is in the known classes, encode it, otherwise set to -1.
             if encoded_data[feature] in encoder.classes_:
                 encoded_value = encoder.transform([encoded_data[feature]])
                 encoded_data[feature] = encoded_value[0]
             else:
                 encoded_data[feature] = -1
         else:
-            # If the feature is empty, also set it to -1.
             encoded_data[feature] = -1
     return encoded_data  # Return the encoded data.
 
@@ -40,47 +35,25 @@ def encode_features(input_data, encoder):
 # Load the LightGBM model from the serialized file.
 model_file_path = 'best_lgbm_model1.pkl'
 with open(model_file_path, 'rb') as file:  # Open the file in read-binary mode.
-    lgbm_model_with_outliers = pickle.load(file)  # Load the trained model.
+    lgbm_model = pickle.load(file)  # Load the trained model.
 
-model_file_path = 'best_lgbm_model_no_outliers1.pkl'
-with open(model_file_path, 'rb') as file:  # Open the file in read-binary mode.
-    lgbm_model_no_outliers = pickle.load(file)  # Load the trained model.
-
-
-# Define a route for the root URL which returns a simple string.
 @app.route('/')
 def home():
     return "LightGBM Model API"
 
-
-# Define the route for making predictions using a POST request.
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)  # Get JSON data from the POST request.
 
     try:
-        # Extract the model_type parameter from the request
-        model_type = data.pop('model_type', None)
-
-        # Select the appropriate model based on model_type
-        if model_type == 'model_1':
-            model = lgbm_model_with_outliers
-        elif model_type == 'model_2':
-            model = lgbm_model_no_outliers
-        else:
-            return jsonify({"error": "Invalid model type"}), 400
-
         # Encode features and make prediction
-        encoded_inputs = encode_features(data, encoder)  # data no longer contains 'model_type'
+        encoded_inputs = encode_features(data, encoder)
         features = np.array([list(encoded_inputs.values())])
-        prediction = model.predict(features)
+        prediction = lgbm_model.predict(features)
         return jsonify(prediction.tolist())
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
-# If this script is run directly, start the Flask web server.
-# Set debug=True for development, so the server will reload on code changes.
 if __name__ == '__main__':
     app.run(debug=True)
